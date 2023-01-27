2c7ff678aced3d2f8507e50a63aecc183fe6d3db
Bruno Malgarise <malgarisebruno@gmail.com>
2023-01-27 00:17:10 -0300

    .

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const titulo = document.querySelector("h1 span"),
 const btn = document.querySelector("#btn"),
     contenedor = document.querySelector(".contenedor");
 
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
 //forEach pora el array Y empezamos a crear el html.
 
 //setTimeout
@@ -23,89 +23,123 @@ const pedirProductos = (arr) => {
     });
 };
 
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
 const renderServicios = (arr) => {
-    productos.forEach((product) => {
-        let contenido = document.createElement("div");
-        contenido.className = "tarjeta";
-        contenido.innerHTML = `
-        <img src="${product.img}">
-        <h3>${product.nombre}</h3>
-        <p class="price">${product.precio} $</p>
-        `;
+    let html;
+    contenedor.innerText = "";
+    for (const item of arr) {
+        const { id, nombre, img, precio } = item;
 
-        contenidoWeb.append(contenido);
-
-        let comprar = document.createElement("button");
-        comprar.innerText = "comprar";
-        comprar.className = "btnComprar"
-
-        contenido.append(comprar);
-
-        comprar.addEventListener("click", () => {
-
-            const repetido = carrito.some((productoRepetido) => productoRepetido.id === product.id);
-
-            if (repetido) {
-                carrito.map((prod) => {
-                    if (prod.id === product.id) {
-                        prod.cantidad++;
-                    }
-                });
-            } else {
-                carrito.push({
-                    id: product.id,
-                    img: product.img,
-                    nombre: product.nombre,
-                    precio: product.precio,
-                    cantidad: product.cantidad,
-                });
-            }
-            console.log(carrito);
-            console.log(carrito.length);
-            carritoContenido();
-            saveLocal();
-        });
+        html = `
+        <div class="tarjeta">
+            <div class="card-image">
+            <img src="${img}">
+            </div>
+        <div class="card-content">
+            <span class="card-title">${nombre.toUpperCase()}</span>
+            <p class="price">$${precio}</p>
+        
+        </div>
+        <div class="card-action">
+            <button class="btn" id="${id}">Comprar</button>
+        </div>
+        </div>
+    `;
+        contenedor.innerHTML += html;
+    }
+        btn.addEventListener("click", () => {
+        console.log(btn);
     });
-};
+}
 
 
 
 
 
-//LocalStorage
-const saveLocal = () => {
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-};
+    // const renderServicios = (arr) => {
+    //     productos.forEach((product) => {
+    //         let contenido = document.createElement("div");
+    //         contenido.className = "tarjeta";
+    //         contenido.innerHTML = `
+    //         <img src="${product.img}">
+    //         <h3>${product.nombre}</h3>
+    //         <p class="price">${product.precio} $</p>
+    //         `;
+
+    //         contenidoWeb.append(contenido);
+
+    //         let comprar = document.createElement("button");
+    //         comprar.innerText = "comprar";
+    //         comprar.className = "btnComprar"
+
+    //         contenido.append(comprar);
+
+    //         comprar.addEventListener("click", () => {
+
+    //             const repetido = carrito.some((productoRepetido) => productoRepetido.id === product.id);
+
+    //             if (repetido) {
+    //                 carrito.map((prod) => {
+    //                     if (prod.id === product.id) {
+    //                         prod.cantidad++;
+    //                     }
+    //                 });
+    //             } else {
+    //                 carrito.push({
+    //                     id: product.id,
+    //                     img: product.img,
+    //                     nombre: product.nombre,
+    //                     precio: product.precio,
+    //                     cantidad: product.cantidad,
+    //                 });
+    //             }
+    //             console.log(carrito);
+    //             console.log(carrito.length);
+    //             carritoContenido();
+    //             saveLocal();
+    //         });
+    //     });
+    // };
 
 
 
-pedirProductos(carrito)
-    .then((res) => {
-        carrito = res;
-        renderServicios(carrito)
-    });
 
-const lista = document.querySelector("#listado");
+    //LocalStorage
+    const saveLocal = () => {
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    };
 
 
 
+    // pedirProductos(carrito)
+    //     .then((res) => {
+    //         carrito = res;
+    //         renderServicios(carrito)
+    //     });
+
+    const lista = document.querySelector("#listado");
 
 
-//FETCH
-fetch('./data/data.json')
-    .then(res => res.json())
-    .then(datos => {
-        console.log(datos);
-        renderServicios(datos);
-    })
 
-data.forEach(post => {
-    const li = document.createElement('li');
-    li.className = 'card';
-    li.innerHTML = `<h2>${post.title}</h2>
+
+
+    //FETCH
+    fetch('./data/data.json')
+        .then(res => res.json())
+        .then(datos => {
+            console.log(datos);
+            renderServicios(datos);
+        })
+
+    data.forEach(post => {
+        const li = document.createElement('li');
+        li.className = 'card';
+        li.innerHTML = `<h2>${post.title}</h2>
             <p>${post.body}</p>`;
-    lista.appendChild(li)
-});
+        lista.appendChild(li)
+    });
 
 
 
